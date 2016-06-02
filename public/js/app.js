69e905c296e90fbb6ea1105f1e5384499aecbaa9
keviveks <keviveks@gmail.com>
2016-06-03 00:05:34 +0530

    added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to socket.io sever!');
 });
+
+socket.on('message', function(message) {
+	console.log('Socket message: ');
+	console.log(message.text);
+});
