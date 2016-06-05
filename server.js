813cc643f0f9a174557804045085622010112394
keviveks <keviveks@gmail.com>
2016-06-05 08:43:12 +0530

    Disconnect room event and message

--- server.js
+++ server.js
@@ -13,6 +13,21 @@ app.use(express.static(__dirname + '/public'));
 io.on('connection', function(socket) {
 	console.log('User connected in socket.io');
 
+	socket.on('disconnect', function() {
+		var userData = clientInfo[socket.id];
+
+		if(typeof userData !== 'undefined') {
+			socket.leave(userData.room);
+			io.to(userData.room).emit('message', {
+				name: 'System',
+				text: userData.name + " has left!",
+				timestamp: now.valueOf()
+			});
+
+			delete clientInfo[socket.id];
+		}
+	});
+
 	socket.on('joinRoom', function(req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
